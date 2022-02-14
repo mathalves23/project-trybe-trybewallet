c3430de996191304d4cdbc1bec3a2b34397532c0
Matheus de Araújo Alves <matheus.aalves@MacBook-Air-de-Matheus.local>
2022-02-14 08:43:40 -0300

    Requisito 7

--- src/components/Despesas.js
+++ src/components/Despesas.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Despesas extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
+      // Source: https://www.w3schools.com/html/html_tables.asp
       <table>
         <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        {expenses ? expenses.map((expense) => { // Lógica realizada com a ajuda da colega Nicole Calderari.
+          const { value, description, currency, method, tag, exchangeRates } = expense;
+          const price = Number(exchangeRates[currency].ask).toFixed(2);
+          const expenseValue = Number(value).toFixed(2);
+          const priceValue = (expenseValue * exchangeRates[currency].ask).toFixed(2);
+          const currencyName = exchangeRates[currency].name.split('/');
+          // Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
+          return (
+            <tr key={ expense.id }>
+              <th>{description}</th>
+              <th>{tag}</th>
+              <th>{method}</th>
+              <th>{value}</th>
+              <th>{currencyName[0]}</th>
+              <th>{price}</th>
+              <th>{priceValue}</th>
+              <th>Real</th>
+            </tr>
+          );
+        }) : null}
       </table>
     );
   }
 }
 
-export default Despesas;
+Despesas.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Despesas);
